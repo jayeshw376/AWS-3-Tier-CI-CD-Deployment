2c7402ab6be622b66b5306b7edde207ef537c560
Jayesh Wagh <jayeshwagh376@gmail.com>
2025-02-13 00:50:19 +0530

    Update all

---
+++ client/src/pages/config.js
@@ -0,0 +1,3 @@
+// const API_BASE_URL = "http://localhost:8800";
+const API_BASE_URL = "http://jayesh.strike2.co.in";
+export default API_BASE_URL;
---
+++ terraform_main_ec2/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "strike1"  # Replace with your S3 bucket name
+    key            = "terraform/state.tfstate"      # The path within the bucket for your state file
+    region         = "us-west-2"                    # AWS region where your bucket is located
+    encrypt        = true                           # Optional: encrypt the state file at rest
+    # dynamodb_table = "terraform-locks"              # Optional: DynamoDB table for state locking
+  }
+}
